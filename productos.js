acacaf7366f03052e5fda2ccc8d5769b128c8d88
deutschmond <47878515+deutschmond@users.noreply.github.com>
2019-11-29 12:52:45 -0600

    I removed all the changes mande with regard to the arreglo class and media queries

--- productos.js
+++ productos.js
@@ -75,7 +75,7 @@ function addItemToCart(title, price,imageSrc){
                                 <span class="cart-item-title">${title}</span>
                             </div>
                             <span class="cart-price cart-column">${price}</span>
-                            <div class="cart-quantity cart-column arreglo">
+                            <div class="cart-quantity cart-column">
                                 <input class="cart-quantity-input" type="number"value="1">
                                 <button class="btn btn-danger" role="button">Quitar</button>
                             </div>
